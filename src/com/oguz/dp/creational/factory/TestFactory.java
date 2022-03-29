b5747c6c33ee64b091aa5fff2bc0caafe367b3a4
oguz.sudas <sudasoguz@gmail.com>
2022-03-30 00:15:48 +0300

    Factory pattern implementation

---
+++ src/com/oguz/dp/creational/factory/TestFactory.java
@@ -0,0 +1,14 @@
+package com.oguz.dp.creational.factory;
+
+public class TestFactory {
+
+    public static void main(String[] args) {
+
+        Computer pc = ComputerFactory.getComputer("pc", "16 GB", "1 TB", "3.6 GHZ");
+        Computer server = ComputerFactory.getComputer("server", "8 GB", "2 TB", "2.4 GHZ");
+
+        System.out.println("PC : " + pc);
+        System.out.println("Server : " + server);
+
+    }
+}
